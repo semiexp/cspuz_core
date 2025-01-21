657841ffbfd7ef34f27ff80adb9feccfb9f5c5a0
semiexp <linthya.v@gmail.com>
2025-01-21 21:52:05 +0900

    Add documentation for graph.rs

--- cspuz_rs/src/graph.rs
+++ cspuz_rs/src/graph.rs
@@ -5,12 +5,16 @@ use super::solver::{
     CSPIntExpr, FromModel, FromOwnedPartialModel, Model, Operand, OwnedPartialModel, Solver, Value,
 };
 
+/// A struct for representing an undirected graph.
 pub struct Graph {
     n_vertices: usize,
     edges: Vec<(usize, usize)>,
 }
 
 impl Graph {
+    /// Constructs a new `Graph` instance with the given number of vertices.
+    /// The vertices are numbered from 0 to `n_vertices - 1`.
+    /// Initially, the graph has no edges.
     pub fn new(n_vertices: usize) -> Graph {
         Graph {
             n_vertices,
@@ -18,20 +22,51 @@ impl Graph {
         }
     }
 
+    /// Adds an edge between the vertices `u` and `v`.
+    ///
+    /// # Examples
+    /// ```
+    /// # use cspuz_rs::graph::Graph;
+    /// let mut graph = Graph::new(5);
+    /// graph.add_edge(0, 1);
+    /// ```
     pub fn add_edge(&mut self, u: usize, v: usize) {
         assert!(u < self.n_vertices);
         assert!(v < self.n_vertices);
         self.edges.push((u, v));
     }
 
+    /// Returns the number of vertices in the graph.
     pub fn n_vertices(&self) -> usize {
         self.n_vertices
     }
 
+    /// Returns the number of edges in the graph.
     pub fn n_edges(&self) -> usize {
         self.edges.len()
     }
 
+    /// Returns the "line graph" of the graph.
+    /// The line graph of a graph is a graph whose vertices correspond to the edges of the original graph,
+    /// and two vertices are connected by an edge if the corresponding edges share a vertex in the original graph.
+    ///
+    /// In the returned graph, the vertices (corresponding to the edges of the original graph) are numbered
+    /// from 0 to `n_edges - 1` in the same order as the edges in the original graph.
+    /// On the other hand, the order of edges in the returned graph is not guaranteed.
+    ///
+    /// # Examples
+    /// ```
+    /// # use cspuz_rs::graph::Graph;
+    /// let mut graph = Graph::new(4);
+    /// graph.add_edge(0, 1);
+    /// graph.add_edge(1, 2);
+    /// graph.add_edge(0, 2);
+    /// graph.add_edge(2, 3);
+    ///
+    /// let line_graph = graph.line_graph();
+    /// assert_eq!(line_graph.n_vertices(), 4);
+    /// assert_eq!(line_graph.n_edges(), 5);
+    /// ```
     pub fn line_graph(&self) -> Graph {
         let mut line_graph = Graph::new(self.n_edges());
         let mut adj = vec![vec![]; self.n_vertices];
@@ -55,6 +90,17 @@ impl Graph {
 impl Index<usize> for Graph {
     type Output = (usize, usize);
 
+    /// Returns the edge at the given index as a pair of vertices.
+    /// Edges are indexed from 0 to `n_edges() - 1` in the order they are added.
+    ///
+    /// # Examples
+    /// ```
+    /// # use cspuz_rs::graph::Graph;
+    /// let mut graph = Graph::new(5);
+    /// graph.add_edge(0, 1);
+    /// graph.add_edge(0, 2);
+    /// assert_eq!(graph[0], (0, 1));
+    /// ```
     fn index(&self, index: usize) -> &Self::Output {
         &self.edges[index]
     }
@@ -77,6 +123,7 @@ fn infer_graph_from_2d_array(shape: (usize, usize)) -> Graph {
 }
 
 /// A struct for maintaining "edges" of a grid, including those on the outer border.
+///
 /// Suppose we have a H * W grid. Then, each cell is surrounded by 2 horizontal edges and 2 vertical edges.
 /// Thus we have (H + 1) * W horizontal edges and H * (W + 1) vertical edges in total.
 ///
@@ -88,18 +135,45 @@ fn infer_graph_from_2d_array(shape: (usize, usize)) -> Graph {
 /// they ignore the orientation of edges: the horizontal line connecting cells (y, x) = (0, 0) and (0, 1) is represented by `vertical[0][0]`.
 /// Thus Masyu answers are better represented by `GridEdges` instances of (H - 1) * (W - 1) grids.
 /// Similarly, edges dividing a grid are better represented by `InnerGridEdges`.
+///
+/// This struct is parameterized by `T`, which represents the type of edges arrays.
+/// For example, `T` can be `BoolVarArray2D` for representing edges as boolean CSP variables.
+/// `T` can also be `Vec<Vec<bool>>` for representing edges as fixed boolean values.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct GridEdges<T> {
+    /// Horizontal edges: top or bottom edges of cells.
+    ///
+    /// `horizontal[y, x]` represents the edge between (y - 1, x) and (y, x).
+    /// If `y == 0`, it represents a top edge of the column `x` of the grid.
+    /// If `y == H`, it represents a bottom edge of the column `x` of the grid.
+    /// `horizontal` has shape (H + 1, W).
     pub horizontal: T,
+
+    /// Vertical edges: left or right edges of cells.
+    ///
+    /// `vertical[y, x]` represents the edge between (y, x - 1) and (y, x).
+    /// If `x == 0`, it represents a left edge of the column `y` of the grid.
+    /// If `x == W`, it represents a right edge of the column `y` of the grid.
+    /// `vertical` has shape (H, W + 1).
     pub vertical: T,
 }
 
 /// A struct for maintaining "inner edges" of a grid. They do not include edges on the outer border.
+///
 /// `InnerGridEdges` for a H * W grid have (H - 1) * W horizontal edges and H * (W - 1) vertical edges.
 /// See the description of `GridEdges` for distinction between `GridEdges` and `InnerGridEdges`.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct InnerGridEdges<T> {
+    /// Horizontal edges: edges between cells of the same column.
+    ///
+    /// `horizontal[y, x]` represents the edge between (y, x) and (y + 1, x).
+    /// `horizontal` has shape (H - 1, W).
     pub horizontal: T,
+
+    /// Vertical edges: edges between cells of the same row.
+    ///
+    /// `vertical[y, x]` represents the edge between (y, x) and (y, x + 1).
+    /// `vertical` has shape (H, W - 1).
     pub vertical: T,
 }
 
@@ -112,6 +186,31 @@ impl<T> InnerGridEdges<Vec<Vec<T>>> {
     }
 }
 
+/// Returns the list of connected components (rooms) from the given `InnerGridEdges`.
+///
+/// Two cells are considered (directly) connected if they share an edge and are not separated by a border
+/// (i.e., the corresponding value in `borders` is `false`).
+/// Then, a room is a maximal set of cells such that any two cells in the room are connected.
+/// The order of rooms in the returned value is not guaranteed.
+/// It is allowed that there exist "extra" borders that do not separate any rooms.
+///
+/// # Examples
+/// ```
+/// # use cspuz_rs::graph::{InnerGridEdges, borders_to_rooms};
+/// let borders = InnerGridEdges {
+///     horizontal: vec![
+///         vec![true, true, false],
+///         vec![false, true, true],
+///     ],
+///     vertical: vec![
+///         vec![true, false],
+///         vec![false, true],
+///         vec![true, false],
+///     ],
+/// };
+/// let rooms = borders_to_rooms(&borders);
+/// assert_eq!(rooms.len(), 4);
+/// ```
 pub fn borders_to_rooms(borders: &InnerGridEdges<Vec<Vec<bool>>>) -> Vec<Vec<(usize, usize)>> {
     fn visit(
         y: usize,
@@ -350,6 +449,41 @@ impl FromOwnedPartialModel for BoolInnerGridEdges {
     }
 }
 
+/// Adds a constraint that "active" vertices in the given graph are connected.
+///
+/// In other words, for any two active vertices, there is a path on `graph` between them
+/// that consists only of active vertices.
+///
+/// # Formal definition
+/// Let `V` be the set of vertices of the graph `graph`, and `E` be the set of edges.
+/// Let `is_active` be a function from `V` to {false, true} that represents whether each vertex is active.
+/// Then, `is_active` defines a subset of `V` by `V' = {v in V | is_active(v)}`.
+/// The constraint requires that the subgraph of `graph` induced by `V'` is connected.
+///
+/// We note that the empty graph is also considered connected here.
+/// Thus, if `is_active` is false for all vertices, the constraint is considered satisfied.
+///
+/// # Examples
+/// ```
+/// # use cspuz_rs::graph::{Graph, active_vertices_connected};
+/// # use cspuz_rs::solver::Solver;
+/// let mut solver = Solver::new();
+/// let is_active = &solver.bool_var_1d(4);
+/// solver.add_expr(is_active.at(0));
+/// solver.add_expr(is_active.at(3));
+///
+/// let mut graph = Graph::new(4);
+/// graph.add_edge(0, 1);
+/// graph.add_edge(1, 2);
+/// graph.add_edge(2, 3);
+///
+/// active_vertices_connected(&mut solver, is_active, &graph);
+///
+/// let answer = solver.solve();
+/// assert!(answer.is_some());
+/// let answer = answer.unwrap();
+/// assert_eq!(answer.get(is_active), vec![true, true, true, true]);
+/// ```
 pub fn active_vertices_connected<T>(solver: &mut Solver, is_active: T, graph: &Graph)
 where
     T: IntoIterator,
@@ -358,6 +492,36 @@ where
     solver.add_active_vertices_connected(is_active, &graph.edges);
 }
 
+/// Adds a constraint that "active" cells in the given 2D grid are connected.
+///
+/// In other words, for any two active cells, there is a path between them that consists only of active cells
+/// and each adjacent pair of cells in the path shares an edge.
+///
+/// We note that, if `is_active` is false for all cells, the constraint is considered satisfied.
+///
+/// This can be seen as a special case of `active_vertices_connected`, where the vertex variables and the graph
+/// are naturally inferred from the shape of the given 2D array.
+///
+/// # Examples
+/// ```
+/// # use cspuz_rs::graph::active_vertices_connected_2d;
+/// # use cspuz_rs::solver::Solver;
+/// let mut solver = Solver::new();
+/// let is_active = &solver.bool_var_2d((2, 2));
+/// solver.add_expr(is_active.at((0, 0)));
+/// solver.add_expr(is_active.at((1, 1)));
+/// solver.add_expr(!is_active.at((0, 1)));
+///
+/// active_vertices_connected_2d(&mut solver, is_active);
+///
+/// let answer = solver.solve();
+/// assert!(answer.is_some());
+/// let answer = answer.unwrap();
+/// assert_eq!(answer.get(is_active), vec![
+///     vec![true, false],
+///     vec![true, true],
+/// ]);
+/// ```
 pub fn active_vertices_connected_2d<T>(solver: &mut Solver, is_active: T)
 where
     T: Operand<Output = Array2DImpl<CSPBoolExpr>>,
@@ -367,6 +531,41 @@ where
     active_vertices_connected(solver, is_active, &graph)
 }
 
+/// Adds a constraint that "active" vertices in the given graph are connected via "active" edges.
+///
+/// In other words, for any two active vertices, there is a path between them that consists only of active vertices
+/// and each adjacent pair of vertices in the path shares an active edge.
+/// This constraint does not require the connectivity of active edges: it is allowed that an active edge
+/// is not connected to any other active vertices.
+///
+/// Note that, if `is_active` is false for all cells in `indices`, the constraint is considered satisfied.
+///
+/// # Examples
+/// ```
+/// # use cspuz_rs::graph::{Graph, active_vertices_connected_via_active_edges};
+/// # use cspuz_rs::solver::Solver;
+/// let mut solver = Solver::new();
+/// let is_active_vertex = &solver.bool_var_1d(4);
+/// solver.add_expr(is_active_vertex.at(0));
+/// solver.add_expr(is_active_vertex.at(3));
+///
+/// let is_active_edge = &solver.bool_var_1d(4);
+/// solver.add_expr(!is_active_edge.at(2));
+///
+/// let mut graph = Graph::new(4);
+/// graph.add_edge(0, 1);
+/// graph.add_edge(1, 2);
+/// graph.add_edge(0, 2);
+/// graph.add_edge(2, 3);
+///
+/// active_vertices_connected_via_active_edges(&mut solver, is_active_vertex, is_active_edge, &graph);
+///
+/// let answer = solver.solve();
+/// assert!(answer.is_some());
+/// let answer = answer.unwrap();
+/// assert_eq!(answer.get(is_active_vertex), vec![true, true, true, true]);
+/// assert_eq!(answer.get(is_active_edge), vec![true, true, false, true]);
+/// ```
 pub fn active_vertices_connected_via_active_edges<T1, T2>(
     solver: &mut Solver,
     is_active_vertex: T1,
@@ -410,6 +609,31 @@ pub fn active_vertices_connected_via_active_edges<T1, T2>(
     active_vertices_connected(solver, &aux_vertices, &aux_graph);
 }
 
+/// Adds a constraint that, in the subset of cells defined by `indices`, "active" cells are connected.
+///
+/// In other words, for any two active cells in `indices`, there is a path between them that consists only of
+/// active cells which are in `indices` and each adjacent pair of cells in the path shares an edge.
+/// Active cells which are not in `indices` are ignored in this constraint.
+/// Cells which are connected only via cells not in `indices` are considered disconnected.
+///
+/// Note that, if `is_active` is false for all cells in `indices`, the constraint is considered satisfied.
+///
+/// # Examples
+/// ```
+/// # use cspuz_rs::graph::active_vertices_connected_2d_region;
+/// # use cspuz_rs::solver::Solver;
+/// let mut solver = Solver::new();
+/// let is_active = &solver.bool_var_2d((2, 2));
+/// solver.add_expr(is_active.at((0, 0)));
+/// solver.add_expr(is_active.at((1, 1)));
+///
+/// active_vertices_connected_2d_region(&mut solver, is_active, &[(0, 0), (0, 1), (1, 1)]);
+///
+/// let answer = solver.solve();
+/// assert!(answer.is_some());
+/// let answer = answer.unwrap();
+/// assert_eq!(answer.get(&is_active.at((0, 1))), true);
+/// ```
 pub fn active_vertices_connected_2d_region<T>(
     solver: &mut Solver,
     is_active: T,
@@ -440,6 +664,34 @@ pub fn active_vertices_connected_2d_region<T>(
     active_vertices_connected(solver, &vertices, &graph)
 }
 
+/// Adds a constraint that "active" edges in the given graph form a single cycle or there is no active edge.
+///
+/// `is_active_edge` defines a subset of edges of `graph` (by selecting edges with `true` values).
+/// The constraint requires that the subset satisfies either of the following conditions:
+/// - The subset forms a single cycle not necessarily spanning all vertices.
+/// - The subset is empty.
+///
+/// # Examples
+/// ```
+/// # use cspuz_rs::graph::{Graph, active_edges_single_cycle};
+/// # use cspuz_rs::solver::Solver;
+/// let mut solver = Solver::new();
+/// let is_active_edge = &solver.bool_var_1d(4);
+/// solver.add_expr(is_active_edge.any());
+///
+/// let mut graph = Graph::new(4);
+/// graph.add_edge(0, 1);
+/// graph.add_edge(1, 2);
+/// graph.add_edge(0, 2);
+/// graph.add_edge(2, 3);
+///
+/// active_edges_single_cycle(&mut solver, is_active_edge, &graph);
+///
+/// let answer = solver.solve();
+/// assert!(answer.is_some());
+/// let answer = answer.unwrap();
+/// assert_eq!(answer.get(is_active_edge), vec![true, true, true, false]);
+/// ```
 pub fn active_edges_single_cycle<T>(
     solver: &mut Solver,
     is_active_edge: T,
@@ -477,6 +729,33 @@ where
     is_passed
 }
 
+/// Adds a constraint that `grid_frame` forms a single cycle or all edges have values of `false`.
+///
+/// Each grid edge connects two grid vertices (a vertex of a grid cell). Then we can construct a graph:
+/// - The vertices of the graph are the grid vertices.
+/// - The edges of the graph are the grid edges.
+///
+/// The constraint requires that the subset of edges defined by `grid_frame` satisfies either of the following conditions:
+/// - The subset forms a single cycle not necessarily spanning all vertices.
+/// - The subset is empty.
+///
+/// # Examples
+/// ```
+/// # use cspuz_rs::graph::{BoolGridEdges, single_cycle_grid_edges};
+/// # use cspuz_rs::solver::Solver;
+/// let mut solver = Solver::new();
+/// let edges = BoolGridEdges::new(&mut solver, (3, 2));
+/// solver.add_expr(!edges.vertical.at((2, 1)));
+/// solver.add_expr(edges.horizontal.at((0, 0)));
+/// solver.add_expr(edges.horizontal.at((3, 0)));
+/// let _ = single_cycle_grid_edges(&mut solver, &edges);
+///
+/// let answer = solver.solve();
+/// assert!(answer.is_some());
+/// let answer = answer.unwrap();
+/// assert_eq!(answer.get(&edges.vertical.at((2, 0))), true);
+/// assert_eq!(answer.get(&edges.vertical.at((2, 2))), true);
+/// ```
 pub fn single_cycle_grid_edges(solver: &mut Solver, grid_frame: &BoolGridEdges) -> BoolVarArray2D {
     let (edges, graph) = grid_frame.representation();
     let is_passed_flat = active_edges_single_cycle(solver, edges, &graph);
