219c3922a532ae3bb70726aef41e0ac9c5c28210
semiexp <linthya.v@gmail.com>
2025-03-21 11:51:01 +0900

    support weight constraint

--- cspuz_core/src/propagators/graph_division.rs
+++ cspuz_core/src/propagators/graph_division.rs
@@ -18,8 +18,8 @@ enum LiteralInfo {
 enum UndoInfo {
     LevelBoundary,
     Edge(usize),
-    LowerBound(usize, i32), // (index, old_value)
-    UpperBound(usize, i32), // (index, old_value)
+    LowerBound(usize, i32, Option<Lit>), // (index, old_value, old_lit)
+    UpperBound(usize, i32, Option<Lit>), // (index, old_value, old_lit)
 }
 
 #[derive(Clone)]
@@ -47,6 +47,8 @@ pub struct GraphDivision {
     num_vertices: usize,
     num_edges: usize,
 
+    vertex_weights: Vec<i32>,
+
     domains: Vec<Vec<i32>>,
     edges: Vec<(usize, usize)>,
     edge_lits: Vec<Lit>,
@@ -55,7 +57,9 @@ pub struct GraphDivision {
 
     edge_state: Vec<EdgeState>,
     lower_bound: Vec<i32>,
+    lower_bound_lit: Vec<Option<Lit>>,
     upper_bound: Vec<i32>,
+    upper_bound_lit: Vec<Option<Lit>>,
 
     unique_lits: Vec<Lit>,
 
@@ -155,6 +159,7 @@ impl GraphDivision {
         GraphDivision {
             num_vertices,
             num_edges,
+            vertex_weights: vertex_weights.to_vec(),
             domains: domains.iter().cloned().collect(),
             edges: edges.iter().cloned().collect(),
             edge_lits: edge_lits.iter().cloned().collect(),
@@ -162,7 +167,9 @@ impl GraphDivision {
             adj,
             edge_state: vec![EdgeState::Undecided; num_edges],
             lower_bound,
+            lower_bound_lit: vec![None; num_vertices],
             upper_bound,
+            upper_bound_lit: vec![None; num_vertices],
             unique_lits,
             propagations: vec![],
             propagation_reasons: vec![Reason::NotPropagated; num_unique_lits],
@@ -187,8 +194,10 @@ impl GraphDivision {
                         self.undo_stack.push(UndoInfo::LowerBound(
                             vertex_idx,
                             self.lower_bound[vertex_idx],
+                            self.lower_bound_lit[vertex_idx],
                         ));
                         self.lower_bound[vertex_idx] = value;
+                        self.lower_bound_lit[vertex_idx] = Some(lit);
 
                         assert!(
                             self.lower_bound[vertex_idx] <= self.upper_bound[vertex_idx],
@@ -203,8 +212,10 @@ impl GraphDivision {
                         self.undo_stack.push(UndoInfo::UpperBound(
                             vertex_idx,
                             self.upper_bound[vertex_idx],
+                            self.upper_bound_lit[vertex_idx],
                         ));
                         self.upper_bound[vertex_idx] = value;
+                        self.upper_bound_lit[vertex_idx] = Some(lit);
 
                         assert!(
                             self.lower_bound[vertex_idx] <= self.upper_bound[vertex_idx],
@@ -228,11 +239,13 @@ impl GraphDivision {
                 UndoInfo::Edge(edge_idx) => {
                     self.edge_state[edge_idx] = EdgeState::Undecided;
                 }
-                UndoInfo::LowerBound(vertex_idx, value) => {
+                UndoInfo::LowerBound(vertex_idx, value, lit) => {
                     self.lower_bound[vertex_idx] = value;
+                    self.lower_bound_lit[vertex_idx] = lit;
                 }
-                UndoInfo::UpperBound(vertex_idx, value) => {
+                UndoInfo::UpperBound(vertex_idx, value, lit) => {
                     self.upper_bound[vertex_idx] = value;
+                    self.upper_bound_lit[vertex_idx] = lit;
                 }
             }
         }
@@ -254,18 +267,18 @@ impl GraphDivision {
         let num_vertices = self.num_vertices;
         let num_edges = self.num_edges;
 
-        let (decided_group_id, decided_groups) = self.compute_groups(false);
-        let (potential_group_id, potential_groups) = self.compute_groups(true);
+        let (decided_region_id, decided_regions) = self.compute_regions(false);
+        let (potential_region_id, potential_regions) = self.compute_regions(true);
 
-        // 1.  Both sides of a "disconnected" edge should be in different groups.
+        // 1.  Both sides of a "disconnected" edge should be in different regions.
         let mut disconnected_groups = vec![];
         for i in 0..num_edges {
             let (u, v) = self.edges[i];
 
-            if decided_group_id[u] == decided_group_id[v] {
+            if decided_region_id[u] == decided_region_id[v] {
                 match self.edge_state[i] {
                     EdgeState::Disconnected => {
-                        let mut reason = self.connected_path(u, v);
+                        let mut reason = self.reason_connected_path(u, v);
                         reason.push(self.edge_lits[i]);
                         self.inconsistency_reason = reason;
                         return false;
@@ -279,8 +292,8 @@ impl GraphDivision {
                     }
                 }
             } else if self.edge_state[i] == EdgeState::Disconnected {
-                let ui = decided_group_id[u];
-                let vi = decided_group_id[v];
+                let ui = decided_region_id[u];
+                let vi = decided_region_id[v];
 
                 if ui > vi {
                     disconnected_groups.push(((vi, ui), i));
@@ -294,8 +307,8 @@ impl GraphDivision {
         for i in 0..num_edges {
             let (u, v) = self.edges[i];
 
-            let ui = decided_group_id[u];
-            let vi = decided_group_id[v];
+            let ui = decided_region_id[u];
+            let vi = decided_region_id[v];
 
             if ui == vi {
                 continue;
@@ -309,37 +322,75 @@ impl GraphDivision {
                 let (_, edge_idx) = disconnected_groups[idx];
 
                 let (eu, ev) = self.edges[edge_idx];
-                assert!(ui == decided_group_id[eu] || ui == decided_group_id[ev]);
-                assert!(vi == decided_group_id[eu] || vi == decided_group_id[ev]);
+                assert!(ui == decided_region_id[eu] || ui == decided_region_id[ev]);
+                assert!(vi == decided_region_id[eu] || vi == decided_region_id[ev]);
 
                 self.register_propagation(
                     self.edge_lits[i],
                     Reason::EdgeBetweenDifferentGroups {
                         disconnected_edge_idx: edge_idx,
                         newly_decided_edge_idx: i,
-                        flip: ui == decided_group_id[ev],
+                        flip: ui == decided_region_id[ev],
                     },
                 );
             }
         }
 
+        // 2. Within a decided region, weight variable must be at least the weight of the region
+        let mut decided_region_weight = vec![];
+        for region in &decided_regions {
+            let weight = region.iter().map(|&i| self.vertex_weights[i]).sum::<i32>();
+            decided_region_weight.push(weight);
+        }
+
+        for i in 0..num_vertices {
+            // NOTE: Vertices with no constraint are ignored because their `upper_bound` is `i32::MAX`.
+            if self.upper_bound[i] < decided_region_weight[decided_region_id[i]] {
+                let mut reason = self.reason_decided_region(i);
+                reason.extend(self.upper_bound_lit[i]);
+                self.inconsistency_reason = reason;
+                return false;
+            }
+        }
+
+        // 3. Within a potential region, weight variable must be at most the weight of the region
+        let mut potential_region_weight = vec![];
+        for region in &potential_regions {
+            let weight = region.iter().map(|&i| self.vertex_weights[i]).sum::<i32>();
+            potential_region_weight.push(weight);
+        }
+
+        for i in 0..num_vertices {
+            // NOTE: Vertices with no constraint are ignored because their `lower_bound` is 0.
+            if self.lower_bound[i] > potential_region_weight[potential_region_id[i]] {
+                // TODO: use DSU-based algorithm to minimize the reason
+                let mut reason = self.reason_potential_region(i);
+                reason.extend(self.lower_bound_lit[i]);
+                self.inconsistency_reason = reason;
+                return false;
+            }
+        }
+
+        // 4. All the weight variables in a decided region must have the same value
+        // TODO: this is not implemented yet
+
         true
     }
 
-    fn compute_groups(&mut self, potential: bool) -> (Vec<usize>, Vec<Vec<usize>>) {
-        let mut group_id = vec![!0; self.domains.len()];
-        let mut groups = vec![];
+    fn compute_regions(&mut self, potential: bool) -> (Vec<usize>, Vec<Vec<usize>>) {
+        let mut region_id = vec![!0; self.domains.len()];
+        let mut regions = vec![];
 
         let mut queue = VecDeque::<usize>::new();
         for i in 0..self.num_vertices {
-            if group_id[i] != !0 {
+            if region_id[i] != !0 {
                 continue;
             }
 
-            let id = groups.len();
+            let id = regions.len();
             let mut group = vec![];
 
-            group_id[i] = id;
+            region_id[i] = id;
             queue.push_back(i);
 
             while let Some(p) = queue.pop_front() {
@@ -352,20 +403,20 @@ impl GraphDivision {
                         continue;
                     }
 
-                    if group_id[q] == !0 {
-                        group_id[q] = id;
+                    if region_id[q] == !0 {
+                        region_id[q] = id;
                         queue.push_back(q);
                     }
                 }
             }
 
-            groups.push(group);
+            regions.push(group);
         }
 
-        (group_id, groups)
+        (region_id, regions)
     }
 
-    fn connected_path(&self, u: usize, v: usize) -> Vec<Lit> {
+    fn reason_connected_path(&self, u: usize, v: usize) -> Vec<Lit> {
         if u == v {
             return vec![];
         }
@@ -394,6 +445,58 @@ impl GraphDivision {
             let (q, lit) = prev[p].unwrap();
             ret.push(!lit); // negated because the edge is connected
             p = q;
+        }
+
+        ret
+    }
+
+    fn reason_decided_region(&self, u: usize) -> Vec<Lit> {
+        let mut queue = VecDeque::<usize>::new();
+        let mut visited = vec![false; self.num_vertices];
+        visited[u] = true;
+        queue.push_back(u);
+
+        let mut ret = vec![];
+
+        // TODO: Use Prim algorithm to find the minimum spanning tree
+        while let Some(p) = queue.pop_front() {
+            for &(q, edge_idx) in &self.adj[p] {
+                if self.edge_state[edge_idx] != EdgeState::Connected {
+                    continue;
+                }
+
+                if !visited[q] {
+                    visited[q] = true;
+                    ret.push(!self.edge_lits[edge_idx]);
+                    queue.push_back(q);
+                }
+            }
+        }
+
+        ret
+    }
+
+    fn reason_potential_region(&self, u: usize) -> Vec<Lit> {
+        let mut queue = VecDeque::<usize>::new();
+        let mut visited = vec![false; self.num_vertices];
+        visited[u] = true;
+        queue.push_back(u);
+
+        let mut ret = vec![];
+
+        // TODO: Use Prim algorithm to find the minimum spanning tree
+        while let Some(p) = queue.pop_front() {
+            for &(q, edge_idx) in &self.adj[p] {
+                if self.edge_state[edge_idx] == EdgeState::Disconnected {
+                    ret.push(self.edge_lits[edge_idx]);
+                    continue;
+                }
+
+                if !visited[q] {
+                    visited[q] = true;
+                    queue.push_back(q);
+                }
+            }
         }
 
         ret
@@ -463,7 +566,7 @@ unsafe impl<T: SolverManipulator> CustomPropagator<T> for GraphDivision {
             &Reason::NotPropagated => panic!(),
             &Reason::EdgeInSameGroup { edge_idx } => {
                 let (u, v) = self.edges[edge_idx];
-                let ret = self.connected_path(u, v);
+                let ret = self.reason_connected_path(u, v);
                 ret
             }
             &Reason::EdgeBetweenDifferentGroups {
@@ -475,8 +578,8 @@ unsafe impl<T: SolverManipulator> CustomPropagator<T> for GraphDivision {
                 let (u2, v2) = self.edges[newly_decided_edge_idx];
                 let (u2, v2) = if flip { (v2, u2) } else { (u2, v2) };
 
-                let mut ret = self.connected_path(u1, u2);
-                let path = self.connected_path(v1, v2);
+                let mut ret = self.reason_connected_path(u1, u2);
+                let path = self.reason_connected_path(v1, v2);
                 ret.extend(path);
                 ret.push(self.edge_lits[disconnected_edge_idx]);
 
@@ -762,6 +865,82 @@ mod tests {
                 None,
                 None,
             ],
+        );
+    }
+
+    #[test]
+    fn test_graph_division_vertex_constraints() {
+        // 2x3 grid graph
+        compare_counts(
+            6,
+            &vec![Some(vec![2, 3]), None, None, None, None, None],
+            None,
+            &[(0, 1), (1, 2), (3, 4), (4, 5), (0, 3), (1, 4), (2, 5)],
+            &[None; 7],
+        );
+
+        // 3x3 grid graph
+        compare_counts(
+            9,
+            &vec![
+                Some(vec![2, 3]),
+                None,
+                None,
+                None,
+                None,
+                Some(vec![1, 3, 4]),
+                None,
+                None,
+                None,
+            ],
+            None,
+            &[
+                (0, 1),
+                (1, 2),
+                (3, 4),
+                (4, 5),
+                (6, 7),
+                (7, 8),
+                (0, 3),
+                (1, 4),
+                (2, 5),
+                (3, 6),
+                (4, 7),
+                (5, 8),
+            ],
+            &[None; 12],
+        );
+
+        // 3x3 grid graph
+        compare_counts(
+            9,
+            &vec![
+                Some(vec![4]),
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                Some(vec![6]),
+            ],
+            None,
+            &[
+                (0, 1),
+                (1, 2),
+                (3, 4),
+                (4, 5),
+                (6, 7),
+                (7, 8),
+                (0, 3),
+                (1, 4),
+                (2, 5),
+                (3, 6),
+                (4, 7),
+                (5, 8),
+            ],
+            &[None; 12],
         );
     }
 }
