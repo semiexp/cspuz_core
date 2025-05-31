a92169d0821f2beaebfe4cd71111715c4c437608
semiexp <linthya.v@gmail.com>
2025-05-31 22:49:37 +0900

    Add solver for Geradeweg

---
+++ cspuz_rs_puzzles/src/puzzles/geradeweg.rs
@@ -0,0 +1,145 @@
+use crate::util;
+use cspuz_rs::graph;
+use cspuz_rs::serializer::{
+    problem_to_url, url_to_problem, Choice, Combinator, Dict, Grid, HexInt, Optionalize, Spaces,
+};
+use cspuz_rs::solver::{Solver, FALSE};
+
+pub fn solve_geradeweg(clues: &[Vec<Option<i32>>]) -> Option<graph::BoolGridEdgesIrrefutableFacts> {
+    let (h, w) = util::infer_shape(clues);
+
+    let mut solver = Solver::new();
+    let is_line = &graph::BoolGridEdges::new(&mut solver, (h - 1, w - 1));
+    solver.add_answer_key_bool(&is_line.horizontal);
+    solver.add_answer_key_bool(&is_line.vertical);
+
+    let is_passed = &graph::single_cycle_grid_edges(&mut solver, &is_line);
+
+    for y in 0..h {
+        for x in 0..w {
+            if let Some(n) = clues[y][x] {
+                solver.add_expr(is_passed.at((y, x)));
+
+                if n > 0 {
+                    let has_left = if x > 0 {
+                        is_line.horizontal.at((y, x - 1)).expr()
+                    } else {
+                        FALSE
+                    };
+                    let has_right = if x < w - 1 {
+                        is_line.horizontal.at((y, x)).expr()
+                    } else {
+                        FALSE
+                    };
+
+                    solver.add_expr(
+                        (has_left | has_right).imp(
+                            (is_line
+                                .horizontal
+                                .slice_fixed_y((y, ..x))
+                                .reverse()
+                                .consecutive_prefix_true()
+                                + is_line
+                                    .horizontal
+                                    .slice_fixed_y((y, x..))
+                                    .consecutive_prefix_true())
+                            .eq(n),
+                        ),
+                    );
+
+                    let has_up = if y > 0 {
+                        is_line.vertical.at((y - 1, x)).expr()
+                    } else {
+                        FALSE
+                    };
+                    let has_down = if y < h - 1 {
+                        is_line.vertical.at((y, x)).expr()
+                    } else {
+                        FALSE
+                    };
+
+                    solver.add_expr(
+                        (has_up | has_down).imp(
+                            (is_line
+                                .vertical
+                                .slice_fixed_x((..y, x))
+                                .reverse()
+                                .consecutive_prefix_true()
+                                + is_line
+                                    .vertical
+                                    .slice_fixed_x((y.., x))
+                                    .consecutive_prefix_true())
+                            .eq(n),
+                        ),
+                    );
+                }
+            }
+        }
+    }
+    solver.irrefutable_facts().map(|f| f.get(is_line))
+}
+
+type Problem = Vec<Vec<Option<i32>>>;
+
+fn combinator() -> impl Combinator<Problem> {
+    Grid::new(Choice::new(vec![
+        Box::new(Optionalize::new(HexInt)),
+        Box::new(Spaces::new(None, 'g')),
+        Box::new(Dict::new(Some(-1), ".")),
+    ]))
+}
+
+pub fn serialize_problem(problem: &Problem) -> Option<String> {
+    problem_to_url(combinator(), "geradeweg", problem.clone())
+}
+
+pub fn deserialize_problem(url: &str) -> Option<Problem> {
+    url_to_problem(combinator(), &["geradeweg"], url)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn problem_for_tests() -> Problem {
+        vec![
+            vec![None, None, None, None, Some(1), None],
+            vec![None, Some(2), Some(3), None, None, None],
+            vec![None, None, None, None, Some(-1), None],
+            vec![None, None, None, None, None, None],
+            vec![None, None, None, None, None, Some(4)],
+        ]
+    }
+
+    #[test]
+    fn test_geradeweg_problem() {
+        let problem = problem_for_tests();
+        let ans = solve_geradeweg(&problem);
+        assert!(ans.is_some());
+        let ans = ans.unwrap();
+
+        let expected = graph::BoolGridEdgesIrrefutableFacts {
+            horizontal: crate::util::tests::to_option_bool_2d([
+                [0, 1, 0, 0, 1],
+                [0, 0, 0, 1, 0],
+                [1, 0, 0, 1, 0],
+                [1, 0, 1, 1, 0],
+                [0, 1, 1, 1, 1],
+            ]),
+            vertical: crate::util::tests::to_option_bool_2d([
+                [0, 1, 1, 0, 1, 1],
+                [0, 1, 1, 1, 0, 1],
+                [1, 0, 1, 0, 1, 1],
+                [0, 1, 0, 0, 0, 1],
+            ]),
+        };
+        assert_eq!(ans, expected);
+    }
+
+    #[test]
+    fn test_geradeweg_serializer() {
+        let problem = problem_for_tests();
+        let url = "https://puzz.link/p?geradeweg/6/5/j1h23m.r4";
+        crate::util::tests::serializer_test(problem, url, serialize_problem, deserialize_problem);
+    }
+}
---
+++ cspuz_solver_backend/src/puzzle/geradeweg.rs
@@ -0,0 +1,32 @@
+use crate::board::{Board, BoardKind, Item, ItemKind};
+use crate::uniqueness::is_unique;
+use cspuz_rs_puzzles::puzzles::geradeweg;
+
+pub fn solve(url: &str) -> Result<Board, &'static str> {
+    let problem = geradeweg::deserialize_problem(url).ok_or("invalid url")?;
+    let is_line = geradeweg::solve_geradeweg(&problem).ok_or("no answer")?;
+
+    let height = problem.len();
+    let width = problem[0].len();
+    let mut board = Board::new(
+        BoardKind::Grid,
+        height,
+        width,
+        is_unique(&is_line),
+    );
+
+    for y in 0..height {
+        for x in 0..width {
+            if let Some(clue) = problem[y][x] {
+                board.push(Item::cell(y, x, "black", ItemKind::Circle));
+                if clue > 0 {
+                    board.push(Item::cell(y, x, "black", ItemKind::Num(clue)));
+                }
+            }
+        }
+    }
+
+    board.add_lines_irrefutable_facts(&is_line, "green", None);
+
+    Ok(board)
+}
