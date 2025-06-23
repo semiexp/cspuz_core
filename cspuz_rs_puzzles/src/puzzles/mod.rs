794352df21bdba8bbcfc9c40a0d56a207a8d031a
Rever <danael.villeneuve@gmail.com>
2025-06-23 16:42:57 -0400

    Add solver for Fourcells

--- cspuz_rs_puzzles/src/puzzles/mod.rs
+++ cspuz_rs_puzzles/src/puzzles/mod.rs
@@ -29,6 +29,7 @@ pub mod firefly;
 pub mod firewalk;
 pub mod fivecells;
 pub mod forestwalk;
+pub mod fourcells;
 pub mod geradeweg;
 pub mod guidearrow;
 pub mod hashi;
---
+++ cspuz_solver_backend/src/puzzle/fourcells.rs
@@ -0,0 +1,83 @@
+use crate::board::{Board, BoardKind, Item, ItemKind};
+use crate::uniqueness::is_unique;
+use cspuz_rs_puzzles::puzzles::fourcells;
+
+pub fn solve(url: &str) -> Result<Board, &'static str> {
+    let problem = fourcells::deserialize_problem(url).ok_or("invalid url")?;
+    let border = fourcells::solve_fourcells(&problem).ok_or("no answer")?;
+
+    let height = problem.len();
+    let width = problem[0].len();
+    let mut board = Board::new(BoardKind::OuterGrid, height, width, is_unique(&border));
+
+    for y in 0..height {
+        for x in 0..width {
+            if let Some(n) = problem[y][x] {
+                if n >= 0 {
+                    board.push(Item::cell(y, x, "black", ItemKind::Num(n)));
+                } else if n == -2 {
+                    board.push(Item::cell(y, x, "black", ItemKind::Fill));
+                }
+            }
+        }
+    }
+
+    for y in 0..height {
+        for x in 0..width {
+            if y < height - 1 {
+                let mut need_default_edge = true;
+                if let Some(b) = border.horizontal[y][x] {
+                    board.push(Item {
+                        y: y * 2 + 2,
+                        x: x * 2 + 1,
+                        color: "green",
+                        kind: if b {
+                            ItemKind::BoldWall
+                        } else {
+                            ItemKind::Cross
+                        },
+                    });
+                    if b {
+                        need_default_edge = false;
+                    }
+                }
+                if need_default_edge {
+                    board.push(Item {
+                        y: y * 2 + 2,
+                        x: x * 2 + 1,
+                        color: "#cccccc",
+                        kind: ItemKind::Wall,
+                    });
+                }
+            }
+            if x < width - 1 {
+                let mut need_default_edge = true;
+                if let Some(b) = border.vertical[y][x] {
+                    board.push(Item {
+                        y: y * 2 + 1,
+                        x: x * 2 + 2,
+                        color: "green",
+                        kind: if b {
+                            ItemKind::BoldWall
+                        } else {
+                            ItemKind::Cross
+                        },
+                    });
+                    if b {
+                        need_default_edge = false;
+                    }
+                }
+                if need_default_edge {
+                    board.push(Item {
+                        y: y * 2 + 1,
+                        x: x * 2 + 2,
+                        color: "#cccccc",
+                        kind: ItemKind::Wall,
+                    });
+                }
+            }
+        }
+    }
+
+    Ok(board)
+}
