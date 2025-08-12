458cfa3784a108ed3046f8f943a8d2311a012331
ReverM <danael.villeneuve@gmail.com>
2025-08-11 21:34:57 -0400

    Fixed some more issues

--- cspuz_rs_puzzles/src/puzzles/putteria.rs
+++ cspuz_rs_puzzles/src/puzzles/putteria.rs
@@ -31,13 +31,13 @@ pub fn solve_putteria(
 
     // Check no duplicates in rows
     for i in 0..h {
-        for j in 1..max_number {
+        for j in 1..(max_number + 1) {
             solver.add_expr(num.slice_fixed_x((.., i)).eq(j).count_true().le(1));
         }
     }
 
     for i in 0..w {
-        for j in 1..max_number {
+        for j in 1..(max_number + 1) {
             solver.add_expr(num.slice_fixed_y((i, ..)).eq(j).count_true().le(1));
         }
     }
@@ -148,7 +148,7 @@ mod tests {
     #[test]
     fn test_putteria_serializer() {
         let problem = problem_for_tests();
-        let url = "https://puzz.link/p?putteria/6/6/mvvuus8o7s83i.zk.l"; // Credits to botaku
+        let url = "https://pzprxs.vercel.app/p?putteria/6/6/mvvuus8o7s83i.zk.l"; // Credits to botaku
         crate::util::tests::serializer_test(problem, url, serialize_problem, deserialize_problem);
     }
 }
