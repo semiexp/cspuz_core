af0bd7ca897f23dc41183fe4db2cef3e5fae04d8
ReverM <danael.villeneuve@gmail.com>
2025-08-15 15:02:30 -0400

    Forgot format

--- cspuz_rs_puzzles/src/puzzles/country_road.rs
+++ cspuz_rs_puzzles/src/puzzles/country_road.rs
@@ -25,7 +25,7 @@ pub fn solve_country_road(
             room_id[y][x] = i;
         }
     }
-    
+
     let mut room_entrance = vec![vec![]; rooms.len()];
     for y in 0..h {
         for x in 0..w {
@@ -44,7 +44,7 @@ pub fn solve_country_road(
         // Check every room is entered only once
         solver.add_expr(count_true(&room_entrance[i]).eq(2));
     }
-    
+
     for y in 0..h {
         for x in 0..w {
             if y < h - 1 && borders.horizontal[y][x] {
@@ -56,7 +56,7 @@ pub fn solve_country_road(
             }
         }
     }
-    
+
     for i in 0..rooms.len() {
         if let Some(n) = clues[i] {
             let mut cells = vec![];
@@ -66,7 +66,7 @@ pub fn solve_country_road(
             solver.add_expr(count_true(cells).eq(n));
         }
     }
-    
+
     solver.irrefutable_facts().map(|f| f.get(is_line))
 }
 
