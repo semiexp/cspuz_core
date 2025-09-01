e37e9d562e10c83c44b5600f0472f0a07eec4d91
ReverM <danael.villeneuve@gmail.com>
2025-09-01 14:33:53 -0400

    (not working) first sketch for Tasquare

---
+++ cspuz_rs_puzzles/src/puzzles/tasquare.rs
@@ -0,0 +1,168 @@
+use crate::util;
+use cspuz_rs::graph;
+use cspuz_rs::serializer::{
+    problem_to_url, url_to_problem, Choice, Combinator, Dict, Grid, HexInt, Optionalize, Spaces,
+};
+use cspuz_rs::solver::{Solver};
+
+pub fn solve_tasquare(clues: &[Vec<Option<i32>>]) -> Option<Vec<Vec<Option<bool>>>> {
+    let (h, w) = util::infer_shape(clues);
+
+    let mut solver = Solver::new();
+    let is_black = &solver.bool_var_2d((h, w));
+    solver.add_answer_key_bool(is_black);
+
+    let widths = &solver.int_var_2d((h, w), 1, w as i32);
+    let heights = &solver.int_var_2d((h, w), 1, h as i32);
+
+    solver.add_answer_key_int(heights);
+    solver.add_answer_key_int(widths);
+
+    for y in 0..h {
+        for x in 0..w {
+            if let Some(n) = clues[y][x] {
+                solver.add_expr(!is_black.at((y,x))); // Clue is not shaded
+                solver.add_expr(is_black.four_neighbors((y, x)).count_true().ge(1)); 
+                if n >= 1 {
+                    let connected = &solver.bool_var_2d((h, w));
+                    for y2 in 0..h {
+                        for x2 in 0..w {
+                            if y == y2 && x == x2 {
+                                solver.add_expr(connected.at((y2, x2)));
+                            } else {
+                                solver.add_expr(connected.at((y2, x2)).imp(is_black.at((y2, x2))));
+                            }
+                        }
+                    }
+                    solver.add_expr(connected.count_true().eq(n + 1));
+                    graph::active_vertices_connected_2d(&mut solver, connected);
+
+                    for nb in connected.four_neighbor_indices((y, x)) {
+                        solver.add_expr(is_black.at(nb).imp(connected.at(nb)));
+                    }
+                    solver.add_expr(
+                        (is_black.slice((1.., ..)) & is_black.slice((..(h - 1), ..))).imp(
+                            connected
+                                .slice((1.., ..))
+                                .iff(connected.slice((..(h - 1), ..))),
+                        ),
+                    );
+                    solver.add_expr(
+                        (is_black.slice((.., 1..)) & is_black.slice((.., ..(w - 1)))).imp(
+                            connected
+                                .slice((.., 1..))
+                                .iff(connected.slice((.., ..(w - 1)))),
+                        ),
+                    );              
+                }
+            }
+        }
+    }
+    
+    for i in 0..w {
+        let slice = is_black.slice_fixed_x((.., i));
+        let mut pos = vec![];
+        let mut count = 0;
+        for j in 0..h {
+            if slice.at(j) {
+                pos.push(j);
+                count += 1;
+            }
+            else {
+                for k in pos {
+                    solver.add_expr(heights.at((i,k)).eq(count));
+                }
+                pos = vec![];
+                count = 0;
+            }
+        }
+    }
+
+    for i in 0..h {
+        let slice = is_black.slice_fixed_y((i, ..));
+        let mut pos = vec![];
+        let mut count = 0;
+        for j in 0..w {
+            if slice.at(j) == Some(true) {
+                pos.push(j);
+                count += 1;
+            }
+            else {
+                for k in pos {
+                    solver.add_expr(widths.at((i,k)).eq(count));
+                }
+                pos = vec![];
+                count = 0;
+            }
+        }
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            solver.add_expr(is_black.at((i,j)).imp(widths.at((i,j)).eq(heights.at((i,j)))));
+        }
+    }
+
+    graph::active_vertices_connected_2d(&mut solver, !is_black);
+
+    solver.irrefutable_facts().map(|f| f.get(is_black))
+}
+
+type Problem = Vec<Vec<Option<i32>>>;
+
+fn combinator() -> impl Combinator<Problem> {
+    Grid::new(Choice::new(vec![
+        Box::new(Optionalize::new(HexInt)),
+        Box::new(Spaces::new(None, 'g')),
+        Box::new(Dict::new(Some(-1), ".")),
+    ]))
+}
+
+pub fn serialize_problem(problem: &Problem) -> Option<String> {
+    problem_to_url(combinator(), "tasquare", problem.clone())
+}
+
+pub fn deserialize_problem(url: &str) -> Option<Problem> {
+    url_to_problem(combinator(), &["tasquare"], url)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[rustfmt::skip]
+    fn problem_for_tests() -> Problem {
+        vec![
+            vec![Some(-1), None, None, Some(4), None, None],
+            vec![None, None, None, None, None, None],
+            vec![Some(-1), None, None, None, Some(5), None],
+            vec![None, Some(3), None, None, None, Some(-1)],
+            vec![None, None, None, None, None, None],
+            vec![None, None, Some(2), None, None, Some(-1)],
+        ]
+    }
+
+    #[test]
+    fn test_tasquare_problem() {
+        let problem = problem_for_tests();
+        let ans = solve_tasquare(&problem);
+        assert!(ans.is_some());
+        let ans = ans.unwrap();
+        let expected = crate::util::tests::to_option_bool_2d([
+            [0, 1, 0, 0, 1, 1],
+            [0, 0, 0, 0, 1, 1],
+            [0, 1, 0, 0, 0, 0],
+            [1, 0, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 1],
+            [0, 1, 0, 1, 0, 0],
+        ]);
+        assert_eq!(ans, expected);
+    }
+
+    #[test]
+    fn test_tasquare_serializer() {
+        let problem = problem_for_tests();
+        let url = "https://puzz.link/p?tasquare/6/6/.h4n.i5h3i.n2h./";
+        util::tests::serializer_test(problem, url, serialize_problem, deserialize_problem);
+    }
+}
---
+++ cspuz_solver_backend/src/puzzle/tasquare.rs
@@ -0,0 +1,32 @@
+use crate::board::{Board, BoardKind, Item, ItemKind};
+use crate::uniqueness::is_unique;
+use cspuz_rs_puzzles::puzzles::tasquare;
+
+pub fn solve(url: &str) -> Result<Board, &'static str> {
+    let problem = tasquare::deserialize_problem(url).ok_or("invalid url")?;
+    let ans = tasquare::solve_tasquare(&problem).ok_or("no answer")?;
+
+    let height = problem.len();
+    let width = problem[0].len();
+    let mut board = Board::new(BoardKind::Grid, height, width, is_unique(&ans));
+    for y in 0..height {
+        for x in 0..width {
+            if let Some(clue) = problem[y][x] {
+                if clue > 0 {
+                    board.push(Item::cell(y, x, "black", ItemKind::Num(clue)));
+                } else {
+                    board.push(Item::cell(y, x, "black", ItemKind::Text("?")));
+                }
+            } else if let Some(a) = ans[y][x] {
+                board.push(Item::cell(
+                    y,
+                    x,
+                    "green",
+                    if a { ItemKind::Block } else { ItemKind::Dot },
+                ));
+            }
+        }
+    }
+
+    Ok(board)
+}
