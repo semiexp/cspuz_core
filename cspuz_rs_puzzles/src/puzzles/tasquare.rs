9bbe6f6597152db7cf281ae42b71ac7ce01571e7
ReverM <danael.villeneuve@gmail.com>
2025-09-01 15:21:10 -0400

    May the rubber duckies be with me

--- cspuz_rs_puzzles/src/puzzles/tasquare.rs
+++ cspuz_rs_puzzles/src/puzzles/tasquare.rs
@@ -59,13 +59,13 @@ pub fn solve_tasquare(clues: &[Vec<Option<i32>>]) -> Option<Vec<Vec<Option<bool>
     solver.add_expr(
         num_up
             .slice((1.., ..))
-            .eq(is_black.ite(num_up.slice((..(h - 1), ..)) + 1, 0)),
+            .eq(is_black.ite(num_up.slice((..h, ..)) + 1, 0)),
     );
     let num_down = &solver.int_var_2d((h, w), 0, h as i32);
     solver.add_expr(num_down.slice_fixed_y((h - 1, ..)).eq(0));
     solver.add_expr(
         num_down
-            .slice((..(h - 1), ..))
+            .slice((..h, ..))
             .eq(is_black.ite(num_down.slice((1.., ..)) + 1, 0)),
     );
     let num_left = &solver.int_var_2d((h, w), 0, w as i32);
@@ -73,13 +73,13 @@ pub fn solve_tasquare(clues: &[Vec<Option<i32>>]) -> Option<Vec<Vec<Option<bool>
     solver.add_expr(
         num_left
             .slice((.., 1..))
-            .eq(is_black.ite(num_left.slice((.., ..(w - 1))) + 1, 0)),
+            .eq(is_black.ite(num_left.slice((.., ..w)) + 1, 0)),
     );
     let num_right = &solver.int_var_2d((h, w), 0, w as i32);
     solver.add_expr(num_right.slice_fixed_x((.., w - 1)).eq(0));
     solver.add_expr(
         num_right
-            .slice((.., ..(w - 1)))
+            .slice((.., ..w))
             .eq(is_black.ite(num_right.slice((.., 1..)) + 1, 0)),
     );
 
