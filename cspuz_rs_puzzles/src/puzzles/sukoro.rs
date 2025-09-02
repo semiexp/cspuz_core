417db3b20b301bd3e79c8af8fdbec3d5d98e0a7c
ReverM <danael.villeneuve@gmail.com>
2025-09-02 11:37:00 -0400

    Testing

--- cspuz_rs_puzzles/src/puzzles/sukoro.rs
+++ cspuz_rs_puzzles/src/puzzles/sukoro.rs
@@ -19,12 +19,12 @@ pub fn solve_sukoro(clues: &[Vec<Option<i32>>]) -> Option<Vec<Vec<Option<i32>>>>
         for x in 0..w {
             solver.add_expr(num.at((y, x)).ne(0));
             solver.add_expr(num.at((y, x)).ne(-1));
-            solver.add_expr(
+           /* solver.add_expr(
                 is_num
                     .four_neighbors((y, x))
                     .count_true()
                     .eq(num.at((y, x))),
-            );
+            );*/
             if let Some(c) = clues[y][x] {
                 if c == -1 {
                     solver.add_expr(num.at((y, x)).ne(-2));
