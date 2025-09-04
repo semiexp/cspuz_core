5f0497812e8cafc18a75deec7e5fb6d3ffe3347a
Danaël V. <104455676+ReverM@users.noreply.github.com>
2025-09-04 11:54:47 -0400

    Update nanro.rs
    Added question marks in the Combinator

--- cspuz_rs_puzzles/src/puzzles/nanro.rs
+++ cspuz_rs_puzzles/src/puzzles/nanro.rs
@@ -89,6 +89,7 @@ fn combinator() -> impl Combinator<Problem> {
         ContextBasedGrid::new(Choice::new(vec![
             Box::new(Optionalize::new(HexInt)),
             Box::new(Spaces::new(None, 'g')),
+            Box::new(Dict::new(Some(-1), ".")),
         ])),
     ))
 }
