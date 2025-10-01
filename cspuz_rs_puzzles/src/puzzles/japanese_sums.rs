427d6c7c92ef6bb4d0348e6937fd993cc6a471e7
semiexp <linthya.v@gmail.com>
2025-10-01 19:42:56 +0900

    support given numbers

--- cspuz_rs_puzzles/src/puzzles/japanese_sums.rs
+++ cspuz_rs_puzzles/src/puzzles/japanese_sums.rs
@@ -1,7 +1,8 @@
 use cspuz_rs::complex_constraints::japanese;
 use cspuz_rs::serializer::{
-    problem_to_url_with_context_and_site, url_to_problem, Choice, Combinator, Context, DecInt,
-    Dict, HexInt, OutsideSequences, PrefixAndSuffix, Size, Tuple2,
+    problem_to_url_with_context_and_site, url_to_problem, Choice, Combinator, Context,
+    ContextBasedGrid, DecInt, Dict, HexInt, Optionalize, OutsideSequences, PrefixAndSuffix, Size,
+    Spaces, Tuple3,
 };
 use cspuz_rs::solver::{IntVarArray1D, Solver};
 
@@ -9,6 +10,7 @@ pub fn solve_japanese_sums(
     k: i32,
     clue_vertical: &[Option<Vec<i32>>],
     clue_horizontal: &[Option<Vec<i32>>],
+    given_numbers: &Option<Vec<Vec<Option<i32>>>>,
 ) -> Option<Vec<Vec<Option<i32>>>> {
     let h = clue_horizontal.len();
     let w = clue_vertical.len();
@@ -41,19 +43,39 @@ pub fn solve_japanese_sums(
     for x in 0..w {
         add_constraint(num.slice_fixed_x((.., x)), &clue_vertical[x]);
     }
+    if let Some(given_numbers) = given_numbers {
+        for y in 0..h {
+            for x in 0..w {
+                if let Some(n) = given_numbers[y][x] {
+                    solver.add_expr(num.at((y, x)).eq(n));
+                }
+            }
+        }
+    }
 
     solver.irrefutable_facts().map(|f| f.get(num))
 }
 
-type Problem = (i32, (Vec<Option<Vec<i32>>>, Vec<Option<Vec<i32>>>));
+type Problem = (
+    i32,
+    (Vec<Option<Vec<i32>>>, Vec<Option<Vec<i32>>>),
+    Option<Vec<Vec<Option<i32>>>>,
+);
 
 fn combinator() -> impl Combinator<Problem> {
-    Size::new(Tuple2::new(
+    Size::new(Tuple3::new(
         PrefixAndSuffix::new("", DecInt, "/"),
         OutsideSequences::new(Choice::new(vec![
             Box::new(Dict::new(-1, ".")),
             Box::new(HexInt),
         ])),
+        Choice::new(vec![
+            Box::new(Optionalize::new(ContextBasedGrid::new(Choice::new(vec![
+                Box::new(Optionalize::new(HexInt)),
+                Box::new(Spaces::new(None, 'g')),
+            ])))),
+            Box::new(Dict::new(None, "")),
+        ]),
     ))
 }
 
@@ -89,18 +111,25 @@ mod tests {
         ];
         let clue_horizontal = vec![
             Some(vec![2, 5, 3]),
-            Some(vec![-1, 4, -1]),
+            Some(vec![-1, -1, -1]),
             Some(vec![-1, 4]),
             None,
             Some(vec![8, -1]),
         ];
-        (4, (clue_vertical, clue_horizontal))
+        let given_numbers = vec![
+            vec![None, None, None, None, None, None],
+            vec![None, None, None, Some(4), None, None],
+            vec![None, None, None, None, None, None],
+            vec![None, None, None, None, None, None],
+            vec![None, None, None, None, None, None],
+        ];
+        (4, (clue_vertical, clue_horizontal), Some(given_numbers))
     }
 
     #[test]
     fn test_japanese_sums_problem() {
-        let (k, problem) = problem_for_tests();
-        let ans = solve_japanese_sums(k, &problem.0, &problem.1);
+        let (k, problem, given_numbers) = problem_for_tests();
+        let ans = solve_japanese_sums(k, &problem.0, &problem.1, &given_numbers);
         assert!(ans.is_some());
         let ans = ans.unwrap();
 
@@ -117,7 +146,7 @@ mod tests {
     #[test]
     fn test_cross_the_streams_serializer() {
         let problem = problem_for_tests();
-        let url = "https://pzprxs.vercel.app/p?japanesesums/6/5/4/...ah5.j.1g.4g352.4.4.j.8g";
+        let url = "https://pzprxs.vercel.app/p?japanesesums/6/5/4/...ah5.j.1g.4g352...4.j.8go4z";
         util::tests::serializer_test(problem, url, serialize_problem, deserialize_problem);
     }
 }
--- cspuz_solver_backend/src/puzzle/japanese_sums.rs
+++ cspuz_solver_backend/src/puzzle/japanese_sums.rs
@@ -3,8 +3,8 @@ use crate::uniqueness::is_unique;
 use cspuz_rs_puzzles::puzzles::japanese_sums;
 
 pub fn solve(url: &str) -> Result<Board, &'static str> {
-    let (k, (vertical, horizontal)) = japanese_sums::deserialize_problem(url).ok_or("invalid url")?;
-    let num = japanese_sums::solve_japanese_sums(k, &vertical, &horizontal).ok_or("no answer")?;
+    let (k, (vertical, horizontal), given_numbers) = japanese_sums::deserialize_problem(url).ok_or("invalid url")?;
+    let num = japanese_sums::solve_japanese_sums(k, &vertical, &horizontal, &given_numbers).ok_or("no answer")?;
 
     let height = num.len();
     let width = num[0].len();
@@ -12,6 +12,12 @@ pub fn solve(url: &str) -> Result<Board, &'static str> {
 
     for y in 0..height {
         for x in 0..width {
+            if let Some(given_numbers) = &given_numbers {
+                if let Some(n) = given_numbers[y][x] {
+                    board.push(Item::cell(y, x, "black", ItemKind::Num(n)));
+                    continue;
+                }
+            }
             if let Some(n) = num[y][x] {
                 board.push(Item::cell(
                     y,
