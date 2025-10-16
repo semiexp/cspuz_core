49251a4a4ca31214ef12b00c2f5d430ea5ce4e6f
ReverM <danael.villeneuve@gmail.com>
2025-10-16 12:28:24 -0400

    Solver is working, Serializer is not

--- cspuz_rs_puzzles/src/puzzles/tents.rs
+++ cspuz_rs_puzzles/src/puzzles/tents.rs
@@ -1,6 +1,8 @@
+use crate::util;
+
 use cspuz_rs::graph;
 use cspuz_rs::serializer::{
-    problem_to_url_with_context, url_to_problem, Choice, Combinator, Context, Dict, HexInt, OutsideSequences, Size, Tuple2,
+    problem_to_url_with_context, url_to_problem, Choice, Combinator, Context, ContextBasedGrid, Dict, HexInt, Map, NumSpaces, OutsideSequences, Size, Spaces, Tuple2,
 };
 use cspuz_rs::solver::Solver;
 
@@ -9,24 +11,23 @@ pub fn solve_tents(
     clue_horizontal: &[Option<Vec<i32>>],
     trees: &Vec<Vec<bool>>,
 ) -> Option<(graph::BoolGridEdgesIrrefutableFacts, Vec<Vec<Option<bool>>>)> {
-    let h = clue_horizontal.len();
-    let w = clue_vertical.len();
+    let (h, w) = util::infer_shape(trees);
 
     let mut solver = Solver::new();
     let is_tent = solver.bool_var_2d((h, w));
     let is_tree = solver.bool_var_2d((h, w));
-    solver.add_answer_key_bool(is_tent);
+    solver.add_answer_key_bool(&is_tent);
 
     let is_pair = &graph::BoolGridEdges::new(&mut solver, (h - 1, w - 1));
-    solver.add_answer_key_bool(&is_line.horizontal);
-    solver.add_answer_key_bool(&is_line.vertical);
+    solver.add_answer_key_bool(&is_pair.horizontal);
+    solver.add_answer_key_bool(&is_pair.vertical);
 
     for x in 0..w {
         for y in 0..h {
+            solver.add_expr((is_tent.at((y, x)) | is_tree.at((y, x))).imp(is_pair.vertex_neighbors((y, x)).count_true().eq(1))); // Each tree is paired with one tent, and vice versa
             if trees[y][x] {
                 solver.add_expr(is_tree.at((y, x)));
                 solver.add_expr(!is_tent.at((y, x)));
-                solver.add_expr((is_tent.at((y, x)) | is_tree.at((y, x))).imp(is_pair.cell_neighbors((y, x)).count_true().eq(1)));
             }
             else{
                 solver.add_expr(!is_tree.at((y, x)));
@@ -34,39 +35,31 @@ pub fn solve_tents(
         }
     }
 
-    solver.add_expr(
-        (!(is_tent.slice((.., ..(w - 1))) & is_tree.slice((.., 1..))) | !(is_tree.slice((.., ..(w - 1))) & is_tent.slice((.., 1..)))).imp(!&is_pair.vertical),
-    );
-    solver.add_expr(
-        (!(is_tent.slice((..(h - 1), ..)) & is_tree.slice((1.., ..))) | !(is_tree.slice((..(h - 1), ..)) & is_tent.slice((1.., ..)))).imp(!&is_pair.horizontal),
-    );
-
-
-    solver.add_expr(!(is_tent.slice((..(height - 1), ..)) & is_tent.slice((1.., ..))));
-    solver.add_expr(!(is_tent.slice((.., ..(height - 1))) & is_tent.slice((.., 1..))));
+    solver.add_expr(!(is_tent.slice((..(h - 1), ..)) & is_tent.slice((1.., ..)))); // Star battle like constraints for tents
+    solver.add_expr(!(is_tent.slice((.., ..(w - 1))) & is_tent.slice((.., 1..))));
     solver
-        .add_expr(!(is_tent.slice((..(height - 1), ..(height - 1))) & is_tent.slice((1.., 1..))));
+        .add_expr(!(is_tent.slice((..(h - 1), ..(w - 1))) & is_tent.slice((1.., 1..))));
     solver
-        .add_expr(!(is_tent.slice((..(height - 1), 1..)) & is_tent.slice((1.., ..(height - 1)))));
+        .add_expr(!(is_tent.slice((..(h - 1), 1..)) & is_tent.slice((1.., ..(w - 1)))));
 
 
     solver.add_expr(is_tree.count_true().eq(is_tent.count_true()));
-    solver.add_expr(is_tent.count_true().eq(&is_pair.horizontal.count_true() + &is_pair.vertical.count_true()));
+    solver.add_expr(is_tent.count_true().eq(&is_pair.horizontal.count_true() + &is_pair.vertical.count_true())); // There are as many pairs as there are tents and trees
 
     
 
     for y in 0..h {
-        if let Some(clue) = &clue_horizontal[y] {
-            solver.add_expr(is_tent.slice_fixed_y((y, ..)).count_true().eq(clue));
+        if let Some(clue) = &clue_vertical[y] {
+            solver.add_expr(is_tent.slice_fixed_y((y, ..)).count_true().eq(clue[0]));
         }
     }
     for x in 0..w {
-        if let Some(clue) = &clue_vertical[x] {
-            solver.add_expr(is_tent.slice_fixed_x((.., x)).count_true().eq(clue));
+        if let Some(clue) = &clue_horizontal[x] {
+            solver.add_expr(is_tent.slice_fixed_x((.., x)).count_true().eq(clue[0]));
         }
     }
 
-    solver.irrefutable_facts().map(|f| (f.get(is_pair), f.get(is_tent)))
+    solver.irrefutable_facts().map(|f| (f.get(is_pair), f.get(&is_tent)))
 }
 
 type Problem = ((Vec<Option<Vec<i32>>>, Vec<Option<Vec<i32>>>), Vec<Vec<bool>>);
@@ -77,21 +70,21 @@ fn combinator() -> impl Combinator<Problem> {
             Box::new(Dict::new(-1, ".")),
             Box::new(HexInt),
         ])),
-           Map::new(
+        ContextBasedGrid::new(Map::new(
             Choice::new(vec![
                 Box::new(NumSpaces::new(0, 17)),
                 Box::new(Spaces::new_with_maximum(None, 'i', 'z')),
             ]),
             |x: bool| match x {
-                true => Some(0),
+                true => Some(Some(0)),
                 false => None,
             },
-            |n: i32| match n {
-                0 => Some(true),
+            |n: Option<i32>| match n {
+                Some(0) => Some(true),
                 _ => Some(false),
             },
         )
-    ))
+    )))
 }
 
 pub fn serialize_problem(problem: &Problem) -> Option<String> {
@@ -117,14 +110,14 @@ mod tests {
     fn problem_for_tests() -> Problem {
         let clue_vertical = vec![
             None,
-            None
+            None,
             Some(vec![1]),
             None,
             None,
         ];
         let clue_horizontal = vec![
             Some(vec![3]),
-            None
+            None,
             Some(vec![2]),
             None,
             None,
@@ -142,19 +135,20 @@ mod tests {
     #[test]
     fn test_tents_problem() {
         let (clues, trees) = problem_for_tests();
-        let ans = solve_japanese_sums(&clues.0, &clues.1, &trees);
+        let ans = solve_tents(&clues.0, &clues.1, &trees);
         assert!(ans.is_some());
-        let ans = ans.unwrap();
+        let (_, is_tent) = ans.unwrap();
 
-        let expected = crate::util::tests::to_option_2d([
-            [2, 0, 4, 1, 0, 3],
-            [0, 3, 0, 4, 0, 1],
-            [4, 2, 0, 3, 1, 0],
-            [0, 1, 2, 0, 0, 4],
-            [1, 4, 3, 0, 2, 0],
+        let expected = crate::util::tests::to_option_bool_2d([
+            [1, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [1, 0, 0, 0, 1],
         ]);
-        assert_eq!(ans, expected);
+        assert_eq!(is_tent, expected);
     }
+
 
     #[test]
     fn test_tents_serializer() {
