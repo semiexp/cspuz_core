22b849a4552274541c22e297a1d6d0b718631625
semiexp <linthya.v@gmail.com>
2025-11-24 02:48:55 +0000

    Add solver for Tetrochain K

---
+++ cspuz_solver_backend/src/puzzle/tetrochain_k.rs
@@ -0,0 +1,86 @@
+use crate::board::{Board, BoardKind, Item, ItemKind};
+use crate::uniqueness::is_unique;
+use cspuz_rs_puzzles::puzzles::tetrochain_k;
+
+pub fn solve(url: &str) -> Result<Board, &'static str> {
+    let problem = tetrochain_k::deserialize_problem(url).ok_or("invalid url")?;
+    let is_black = tetrochain_k::solve_tetrochain_k(&problem).ok_or("no answer")?;
+    let height = (problem.len() + 1) / 2;
+    let width = (problem[0].len() + 1) / 2;
+    let mut board = Board::new(BoardKind::OuterGrid, height, width, is_unique(&is_black));
+
+    for y in 0..height {
+        for x in 0..width {
+            match is_black[y][x] {
+                Some(true) => {
+                    board.push(Item::cell(y, x, "green", ItemKind::Fill));
+                }
+                Some(false) => {
+                    if problem[y * 2][x * 2] == 0 {
+                        board.push(Item::cell(y, x, "green", ItemKind::Dot));
+                    }
+                }
+                None => {}
+            }
+        }
+    }
+
+    for y in 0..height {
+        for x in 0..(width - 1) {
+            board.push(Item {
+                y: y * 2 + 1,
+                x: x * 2 + 2,
+                color: "black",
+                kind: ItemKind::Wall,
+            });
+        }
+    }
+    for y in 0..(height - 1) {
+        for x in 0..width {
+            board.push(Item {
+                y: y * 2 + 2,
+                x: x * 2 + 1,
+                color: "black",
+                kind: ItemKind::Wall,
+            });
+        }
+    }
+
+    for y in 0..(height * 2 - 1) {
+        for x in 0..(width * 2 - 1) {
+            if problem[y][x] == 0 {
+                continue;
+            }
+            if problem[y][x] == 1 {
+                board.push(Item {
+                    y: y + 1,
+                    x: x + 1,
+                    color: "black",
+                    kind: ItemKind::SmallFilledCircle,
+                });
+            } else if problem[y][x] == 2 {
+                board.push(Item {
+                    y: y + 1,
+                    x: x + 1,
+                    color: "#cccccc",
+                    kind: ItemKind::SmallFilledCircle,
+                });
+            } else if problem[y][x] == 3 {
+                board.push(Item {
+                    y: y + 1,
+                    x: x + 1,
+                    color: "white",
+                    kind: ItemKind::SmallFilledCircle,
+                });
+            }
+            board.push(Item {
+                y: y + 1,
+                x: x + 1,
+                color: "black",
+                kind: ItemKind::SmallCircle,
+            });
+        }
+    }
+
+    Ok(board)
+}
