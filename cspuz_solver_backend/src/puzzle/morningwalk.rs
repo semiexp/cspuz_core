3d98e24868b798394adfe8703735e8a3d703bcd1
semiexp <linthya.v@gmail.com>
2025-11-24 22:13:18 +0900

    Add solver for Morning Walk (#146)

---
+++ cspuz_solver_backend/src/puzzle/morningwalk.rs
@@ -0,0 +1,30 @@
+use crate::board::{Board, BoardKind, Item, ItemKind};
+use crate::uniqueness::is_unique;
+use cspuz_rs_puzzles::puzzles::morningwalk;
+
+pub fn solve(url: &str) -> Result<Board, &'static str> {
+    let (forest, num) = morningwalk::deserialize_problem(url).ok_or("invalid url")?;
+    let is_line = morningwalk::solve_morningwalk(&forest, &num).ok_or("no answer")?;
+    let height = forest.len();
+    let width = forest[0].len();
+    let mut board = Board::new(BoardKind::Grid, height, width, is_unique(&is_line));
+
+    for y in 0..height {
+        for x in 0..width {
+            if forest[y][x] {
+                board.push(Item::cell(y, x, "#e0d0ff", ItemKind::Fill));
+            }
+            if let Some(n) = num[y][x] {
+                if n >= 0 {
+                    board.push(Item::cell(y, x, "black", ItemKind::Num(n)));
+                } else {
+                    board.push(Item::cell(y, x, "black", ItemKind::Text("?")));
+                }
+            }
+        }
+    }
+
+    board.add_lines_irrefutable_facts(&is_line, "green", None);
+
+    Ok(board)
+}
