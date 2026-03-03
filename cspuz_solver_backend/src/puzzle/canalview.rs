e36145b88d5824a7cef2a32ad322c99c9d4a84c4
ReverM <danael.villeneuve@gmail.com>
2026-03-03 11:45:20 -0500

    Added solver for canal view

---
+++ cspuz_solver_backend/src/puzzle/canalview.rs
@@ -0,0 +1,94 @@
+use crate::board::{Board, BoardKind, Item, ItemKind};
+use crate::uniqueness::check_uniqueness;
+use cspuz_rs_puzzles::puzzles::canalview;
+
+pub fn solve(url: &str) -> Result<Board, &'static str> {
+    let problem = canalview::deserialize_problem(url).ok_or("invalid url")?;
+    let ans = canalview::solve_canalview(&problem);
+
+    let height = problem.len();
+    let width = problem[0].len();
+    let mut board = Board::new(BoardKind::Grid, height, width, check_uniqueness(&ans));
+    for y in 0..height {
+        for x in 0..width {
+            if let Some(clue) = problem[y][x] {
+                if clue > 0 {
+                    board.push(Item::cell(y, x, "black", ItemKind::Num(clue)));
+                } else {
+                    board.push(Item::cell(y, x, "black", ItemKind::Text("?")));
+                }
+            } else if let Some(ans) = &ans {
+                if let Some(a) = ans[y][x] {
+                    board.push(Item::cell(
+                        y,
+                        x,
+                        "green",
+                        if a { ItemKind::Block } else { ItemKind::Dot },
+                    ));
+                }
+            }
+        }
+    }
+
+    Ok(board)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+    use crate::board::*;
+    use crate::compare_board_and_check_no_solution_case;
+    use crate::uniqueness::Uniqueness;
+
+    #[test]
+    #[rustfmt::skip]
+    fn test_solve() {
+        compare_board_and_check_no_solution_case!(
+            solve("https://puzz.link/p?canal/6/6/3i3h2z4h5i2"),
+            Board {
+                kind: BoardKind::Grid,
+                height: 6,
+                width: 6,
+                data: vec![
+                    Item { y: 1, x: 1, color: "black", kind: ItemKind::Num(3) },
+                    Item { y: 1, x: 3, color: "green", kind: ItemKind::Block },
+                    Item { y: 1, x: 5, color: "green", kind: ItemKind::Block },                   
+                    Item { y: 1, x: 7, color: "green", kind: ItemKind::Block },
+                    Item { y: 1, x: 9, color: "black", kind: ItemKind::Num(3) },
+                    Item { y: 1, x: 11, color: "green", kind: ItemKind::Dot },
+                    Item { y: 3, x: 1, color: "green", kind: ItemKind::Dot },
+                    Item { y: 3, x: 3, color: "black", kind: ItemKind::Num(2) },
+                    Item { y: 3, x: 5, color: "green", kind: ItemKind::Block },                   
+                    Item { y: 3, x: 7, color: "green", kind: ItemKind::Dot },
+                    Item { y: 3, x: 9, color: "green", kind: ItemKind::Dot },
+                    Item { y: 3, x: 11, color: "green", kind: ItemKind::Dot },
+                    Item { y: 5, x: 1, color: "green", kind: ItemKind::Dot },
+                    Item { y: 5, x: 3, color: "green", kind: ItemKind::Dot },
+                    Item { y: 5, x: 5, color: "green", kind: ItemKind::Block },                   
+                    Item { y: 5, x: 7, color: "green", kind: ItemKind::Dot },
+                    Item { y: 5, x: 9, color: "green", kind: ItemKind::Block },
+                    Item { y: 5, x: 11, color: "green", kind: ItemKind::Dot },
+                    Item { y: 7, x: 1, color: "green", kind: ItemKind::Dot },
+                    Item { y: 7, x: 3, color: "green", kind: ItemKind::Block },
+                    Item { y: 7, x: 5, color: "green", kind: ItemKind::Block },                   
+                    Item { y: 7, x: 7, color: "green", kind: ItemKind::Block },
+                    Item { y: 7, x: 9, color: "green", kind: ItemKind::Block },
+                    Item { y: 7, x: 11, color: "green", kind: ItemKind::Block },
+                    Item { y: 9, x: 1, color: "green", kind: ItemKind::Block },
+                    Item { y: 9, x: 3, color: "green", kind: ItemKind::Block },
+                    Item { y: 9, x: 5, color: "green", kind: ItemKind::Dot },                   
+                    Item { y: 9, x: 7, color: "green", kind: ItemKind::Block },
+                    Item { y: 9, x: 9, color: "black", kind: ItemKind::Num(4) },
+                    Item { y: 9, x: 11, color: "green", kind: ItemKind::Block },
+                    Item { y: 11, x: 1, color: "green", kind: ItemKind::Block },
+                    Item { y: 11, x: 3, color: "black", kind: ItemKind::Num(5) },
+                    Item { y: 11, x: 5, color: "green", kind: ItemKind::Block },                   
+                    Item { y: 11, x: 7, color: "green", kind: ItemKind::Block },
+                    Item { y: 11, x: 9, color: "green", kind: ItemKind::Dot },
+                    Item { y: 11, x: 11, color: "black", kind: ItemKind::Num(2) },
+                ],
+                uniqueness: Uniqueness::Unique,
+            },
+        );
+    }
+}
