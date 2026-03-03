3353f2f33d828afc9937c9f032ae3150166dad84
ReverM <danael.villeneuve@gmail.com>
2026-03-03 11:04:14 -0500

    Added solver for simple gako

---
+++ cspuz_solver_backend/src/puzzle/simplegako.rs
@@ -0,0 +1,70 @@
+use crate::board::{Board, BoardKind, Item, ItemKind};
+use crate::uniqueness::check_uniqueness;
+use cspuz_rs_puzzles::puzzles::simplegako;
+
+pub fn solve(url: &str) -> Result<Board, &'static str> {
+    let problem = simplegako::deserialize_problem(url).ok_or("invalid url")?;
+    let ans = simplegako::solve_simplegako(&problem);
+
+    let height = problem.len();
+    let width = problem[0].len();
+    let mut board = Board::new(BoardKind::Grid, height, width, check_uniqueness(&ans));
+
+    for y in 0..height {
+        for x in 0..width {
+            if let Some(n) = problem[y][x] {
+                if n >= 0 {
+                    board.push(Item::cell(y, x, "black", ItemKind::Num(n)));
+                } else {
+                    board.push(Item::cell(y, x, "black", ItemKind::Text("?")));
+                }
+            } else if let Some(ans) = &ans {
+                if let Some(n) = ans[y][x] {
+                    board.push(Item::cell(y, x, "green", ItemKind::Num(n)));
+                }
+            }
+        }
+    }
+
+    Ok(board)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+    use crate::board::*;
+    use crate::compare_board_and_check_no_solution_case;
+    use crate::uniqueness::Uniqueness;
+
+    #[test]
+    #[rustfmt::skip]
+    fn test_solve() {
+        compare_board_and_check_no_solution_case!(
+            solve("https://puzz.link/p?simplegako/4/4/k5g12g2k"),
+            Board {
+                kind: BoardKind::Grid,
+                height: 4,
+                width: 4,
+                data: vec![
+                    Item { y: 1, x: 1, color: "green", kind: ItemKind::Num(5) },
+                    Item { y: 1, x: 3, color: "green", kind: ItemKind::Num(5) },
+                    Item { y: 1, x: 5, color: "green", kind: ItemKind::Num(5) },
+                    Item { y: 1, x: 7, color: "green", kind: ItemKind::Num(3) },
+                    Item { y: 3, x: 1, color: "green", kind: ItemKind::Num(5) },
+                    Item { y: 3, x: 3, color: "black", kind: ItemKind::Num(5) },
+                    Item { y: 3, x: 5, color: "green", kind: ItemKind::Num(5) },
+                    Item { y: 3, x: 7, color: "black", kind: ItemKind::Num(1) },
+                    Item { y: 5, x: 1, color: "black", kind: ItemKind::Num(2) },
+                    Item { y: 5, x: 3, color: "green", kind: ItemKind::Num(1) },
+                    Item { y: 5, x: 5, color: "black", kind: ItemKind::Num(2) },
+                    Item { y: 5, x: 7, color: "green", kind: ItemKind::Num(3) },
+                    Item { y: 7, x: 1, color: "green", kind: ItemKind::Num(5) },
+                    Item { y: 7, x: 3, color: "green", kind: ItemKind::Num(5) },
+                    Item { y: 7, x: 5, color: "green", kind: ItemKind::Num(5) },
+                    Item { y: 7, x: 7, color: "green", kind: ItemKind::Num(3) },
+                ],
+                uniqueness: Uniqueness::Unique,
+            },
+        );
+    }
+}
