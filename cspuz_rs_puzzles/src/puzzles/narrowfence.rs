ac33687d3d2e08d3fdc71e58f2bd0eed0d06fbf7
semiexp <linthya.v@gmail.com>
2026-04-22 11:13:37 +0000

    Add solver for Narrowfence

---
+++ cspuz_rs_puzzles/src/puzzles/narrowfence.rs
@@ -0,0 +1,196 @@
+use crate::util;
+use cspuz_rs::different_shape::DifferentShape;
+use cspuz_rs::graph;
+use cspuz_rs::serializer::{
+    problem_to_url_pzprxs, url_to_problem, Choice, Combinator, Dict, Grid, HexInt, Optionalize,
+    Spaces,
+};
+use cspuz_rs::solver::{any, Solver};
+
+pub fn solve_narrowfence(
+    clues: &[Vec<Option<i32>>],
+) -> Option<graph::BoolInnerGridEdgesIrrefutableFacts> {
+    let (h, w) = util::infer_shape(clues);
+
+    let mut solver = Solver::new();
+    let is_border = graph::BoolInnerGridEdges::new(&mut solver, (h, w));
+    solver.add_answer_key_bool(&is_border.horizontal);
+    solver.add_answer_key_bool(&is_border.vertical);
+
+    let mut max_clue = 0;
+    for y in 0..h {
+        for x in 0..w {
+            if let Some(n) = clues[y][x] {
+                max_clue = max_clue.max(n);
+            }
+        }
+    }
+    if max_clue == 0 {
+        return None;
+    }
+
+    let mut clue_groups = vec![vec![]; (max_clue + 1) as usize];
+    let mut id = 0;
+    for y in 0..h {
+        for x in 0..w {
+            if let Some(n) = clues[y][x] {
+                clue_groups[n as usize].push((y, x, id));
+                id += 1;
+            }
+        }
+    }
+
+    let block_id = &solver.int_var_2d((h, w), 0, id - 1);
+    solver.add_expr(
+        block_id
+            .slice((.., ..(w - 1)))
+            .ne(block_id.slice((.., 1..)))
+            .iff(&is_border.vertical),
+    );
+    solver.add_expr(
+        block_id
+            .slice((..(h - 1), ..))
+            .ne(block_id.slice((1.., ..)))
+            .iff(&is_border.horizontal),
+    );
+
+    for i in 0..=max_clue as usize {
+        for &(y, x, id) in &clue_groups[i] {
+            solver.add_expr(block_id.at((y, x)).eq(id));
+        }
+    }
+
+    for i in 0..=max_clue as usize {
+        if clue_groups[i].is_empty() {
+            continue;
+        }
+
+        let mut indicators = vec![];
+        for &(_, _, id) in &clue_groups[i] {
+            indicators.push(block_id.eq(id));
+        }
+
+        for ind in &indicators {
+            graph::active_vertices_connected_2d(&mut solver, ind);
+        }
+
+        // do not allow 2x2 blocks in the same clue group
+        {
+            let mut in_group = indicators[0].clone();
+            for j in 1..indicators.len() {
+                in_group = in_group | indicators[j].clone();
+            }
+            solver.add_expr(!(in_group.conv2d_and((2, 2))));
+        }
+
+        // not rectangles
+        {
+            for ind in &indicators {
+                let mut cand = vec![];
+                for y in 0..(h - 1) {
+                    for x in 0..(w - 1) {
+                        cand.push(ind.slice((y..(y + 2), x..(x + 2))).count_true().eq(3));
+                    }
+                }
+                solver.add_expr(any(cand));
+            }
+        }
+
+        for u in 0..indicators.len() {
+            for v in (u + 1)..indicators.len() {
+                let mut c = vec![];
+                for y in 0..h {
+                    for x in 0..w {
+                        c.push(indicators[u].at((y, x)));
+                    }
+                }
+                for y in 0..h {
+                    for x in 0..w {
+                        c.push(indicators[v].at((y, x)));
+                    }
+                }
+
+                #[cfg(not(test))]
+                {
+                    solver.add_custom_constraint(Box::new(DifferentShape::new(h, w)), c);
+                }
+
+                #[cfg(test)]
+                {
+                    solver.add_custom_constraint(
+                        Box::new(util::tests::ReasonVerifier::new(
+                            DifferentShape::new(h, w),
+                            DifferentShape::new(h, w),
+                        )),
+                        c,
+                    );
+                }
+            }
+        }
+    }
+
+    solver.irrefutable_facts().map(|f| f.get(&is_border))
+}
+
+type Problem = Vec<Vec<Option<i32>>>;
+
+fn combinator() -> impl Combinator<Problem> {
+    Grid::new(Choice::new(vec![
+        Box::new(Optionalize::new(HexInt)),
+        Box::new(Spaces::new(None, 'g')),
+        Box::new(Dict::new(Some(-1), ".")),
+    ]))
+}
+
+pub fn serialize_problem(problem: &Problem) -> Option<String> {
+    problem_to_url_pzprxs(combinator(), "narrow", problem.clone())
+}
+
+pub fn deserialize_problem(url: &str) -> Option<Problem> {
+    url_to_problem(combinator(), &["narrow"], url)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn problem_for_tests() -> Problem {
+        vec![
+            vec![Some(1), Some(1), None, Some(1), None],
+            vec![None, None, None, None, None],
+            vec![None, None, Some(2), Some(2), None],
+            vec![None, None, None, None, None],
+        ]
+    }
+
+    #[test]
+    fn test_narrowfence_problem() {
+        let problem = problem_for_tests();
+        let ans = solve_narrowfence(&problem);
+        assert!(ans.is_some());
+        let ans = ans.unwrap();
+
+        let expected = graph::BoolInnerGridEdgesIrrefutableFacts {
+            horizontal: crate::util::tests::to_option_bool_2d([
+                [0, 1, 0, 1, 0],
+                [0, 0, 1, 0, 0],
+                [0, 1, 1, 0, 1],
+            ]),
+            vertical: crate::util::tests::to_option_bool_2d([
+                [1, 0, 1, 0],
+                [1, 1, 1, 1],
+                [1, 0, 1, 1],
+                [0, 0, 1, 0],
+            ]),
+        };
+
+        assert_eq!(ans, expected);
+    }
+
+    #[test]
+    fn test_narrowfence_serializer() {
+        let problem = problem_for_tests();
+        let url = "https://pzprxs.vercel.app/p?narrow/5/4/11g1n22l";
+        util::tests::serializer_test(problem, url, serialize_problem, deserialize_problem);
+    }
+}
---
+++ cspuz_solver_backend/src/puzzle/narrowfence.rs
@@ -0,0 +1,95 @@
+use crate::board::{Board, BoardKind, Item, ItemKind};
+use crate::uniqueness::check_uniqueness;
+use cspuz_rs_puzzles::puzzles::narrowfence;
+
+const ALPHA: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+pub fn solve(url: &str) -> Result<Board, &'static str> {
+    let problem = narrowfence::deserialize_problem(url).ok_or("invalid url")?;
+    let border = narrowfence::solve_narrowfence(&problem);
+
+    let height = problem.len();
+    let width = problem[0].len();
+    let mut board = Board::new(
+        BoardKind::ColoredGrid("#cccccc"),
+        height,
+        width,
+        check_uniqueness(&border),
+    );
+    for y in 0..height {
+        for x in 0..width {
+            if let Some(clue) = problem[y][x] {
+                if (1..=26).contains(&clue) {
+                    let p = (clue - 1) as usize;
+                    board.push(Item::cell(y, x, "black", ItemKind::Text(&ALPHA[p..=p])));
+                } else {
+                    board.push(Item::cell(y, x, "black", ItemKind::Num(clue - 26)));
+                }
+            }
+        }
+    }
+
+    board.add_borders_as_answer(border.as_ref());
+
+    Ok(board)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+    use crate::board::*;
+    use crate::compare_board_and_check_no_solution_case;
+    use crate::uniqueness::Uniqueness;
+
+    #[test]
+    #[rustfmt::skip]
+    fn test_solve() {
+        compare_board_and_check_no_solution_case!(
+            solve("https://pzprxs.vercel.app/p?narrow/5/4/11g1n22l"),
+            Board {
+                kind: BoardKind::ColoredGrid("#cccccc"),
+                height: 4,
+                width: 5,
+                data: vec![
+                    Item { y: 1, x: 1, color: "black", kind: ItemKind::Text("A") },
+                    Item { y: 1, x: 3, color: "black", kind: ItemKind::Text("A") },
+                    Item { y: 1, x: 7, color: "black", kind: ItemKind::Text("A") },
+                    Item { y: 5, x: 5, color: "black", kind: ItemKind::Text("B") },
+                    Item { y: 5, x: 7, color: "black", kind: ItemKind::Text("B") },
+                    Item { y: 2, x: 1, color: "green", kind: ItemKind::Cross },
+                    Item { y: 1, x: 2, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 2, x: 3, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 1, x: 4, color: "green", kind: ItemKind::Cross },
+                    Item { y: 2, x: 5, color: "green", kind: ItemKind::Cross },
+                    Item { y: 1, x: 6, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 2, x: 7, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 1, x: 8, color: "green", kind: ItemKind::Cross },
+                    Item { y: 2, x: 9, color: "green", kind: ItemKind::Cross },
+                    Item { y: 4, x: 1, color: "green", kind: ItemKind::Cross },
+                    Item { y: 3, x: 2, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 4, x: 3, color: "green", kind: ItemKind::Cross },
+                    Item { y: 3, x: 4, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 4, x: 5, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 3, x: 6, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 4, x: 7, color: "green", kind: ItemKind::Cross },
+                    Item { y: 3, x: 8, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 4, x: 9, color: "green", kind: ItemKind::Cross },
+                    Item { y: 6, x: 1, color: "green", kind: ItemKind::Cross },
+                    Item { y: 5, x: 2, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 6, x: 3, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 5, x: 4, color: "green", kind: ItemKind::Cross },
+                    Item { y: 6, x: 5, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 5, x: 6, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 6, x: 7, color: "green", kind: ItemKind::Cross },
+                    Item { y: 5, x: 8, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 6, x: 9, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 7, x: 2, color: "green", kind: ItemKind::Cross },
+                    Item { y: 7, x: 4, color: "green", kind: ItemKind::Cross },
+                    Item { y: 7, x: 6, color: "green", kind: ItemKind::BoldWall },
+                    Item { y: 7, x: 8, color: "green", kind: ItemKind::Cross },
+                ],
+                uniqueness: Uniqueness::Unique,
+            },
+        );
+    }
+}
